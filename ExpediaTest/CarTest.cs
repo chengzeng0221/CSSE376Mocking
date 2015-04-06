a3b3cfbfa4bc479475780f000e9cfa14204f230f
CHENG ZENG <zengc@rose-hulman.edu>
2015-04-05 21:56:08 -0400

    Lab3 Code part

--- ExpediaTest/CarTest.cs
+++ ExpediaTest/CarTest.cs
@@ -1,6 +1,7 @@
 using System;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Expedia;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
 using Rhino.Mocks;
 
 namespace ExpediaTest
@@ -50,5 +51,58 @@ namespace ExpediaTest
 		{
 			new Car(-5);
 		}
+
+        [TestMethod]
+        public void TestThatCarDoesGetLocationFromDatabase()
+        {
+            IDatabase mockDB = mocks.StrictMock<IDatabase>();
+            String carLocation = "Terre Haute";
+            String anotherCarLocation = "Indy";
+
+            Expect.Call(mockDB.getCarLocation(10)).Return(carLocation);
+            Expect.Call(mockDB.getCarLocation(101)).Return(anotherCarLocation);
+
+            mocks.ReplayAll();
+
+            Car target = new Car(10);
+            target.Database = mockDB;
+            String result;
+
+            result = target.getCarLocation(101);
+            Assert.AreEqual(anotherCarLocation, result);
+
+            result = target.getCarLocation(10);
+            Assert.AreEqual(carLocation, result);
+
+            mocks.VerifyAll();
+        }
+
+        [TestMethod]
+        public void TestThatCarDoesGetMileageFromDatabase()
+        {
+            IDatabase mockDatabase = mocks.StrictMock<IDatabase>();
+            Int32 Miles = 100;
+
+            Expect.Call(mockDatabase.Miles).PropertyBehavior();
+
+            mocks.ReplayAll();
+
+            mockDatabase.Miles = Miles;
+
+            var target = new Car(10);
+            target.Database = mockDatabase;
+
+            int mileage = target.Mileage;
+            Assert.AreEqual(mileage, Miles);
+
+            mocks.VerifyAll();
+        }
+
+        [TestMethod]
+        public void TestThatBMWCarHasCorrectBasePriceForTenDays()
+        {
+            var target = ObjectMother.BMW();
+            Assert.AreEqual(80, target.getBasePrice());
+        }
 	}
 }
